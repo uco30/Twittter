5e41dd36d12852161a036e0a65fcd06ff65eb39f
Marcos Vives <marcoshvives@gmail.com>
2020-08-19 20:50:09 +0200

    finished tweaking the show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweeets
+  has_many :tweeets, dependent: :destroy
 end
