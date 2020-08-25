42d082a148ee96fec862c5ea09f345e0991a509b
Marcos Vives <marcoshvives@gmail.com>
2020-08-25 15:33:06 +0200

    rerout to root path after creating and deleting comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,13 @@ class CommentsController < ApplicationController
     @comment = @tweeet.comments.create(params[:comment].permit(:name, :body, :user_id))
     @comment.user_id = current_user.id
     @comment.save
-    redirect_to tweeet_path(@tweeet)
+    redirect_to root_path
   end
 
   def destroy
     @tweeet = Tweeet.find(params[:tweeet_id])
     @comment = @tweeet.comments.find(params[:id])
     @comment.destroy
-    redirect_to tweeet_path(@tweeet)
+    redirect_to root_path
   end
 end
